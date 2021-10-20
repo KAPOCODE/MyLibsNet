20aa929058f9d326b4ee539cfe44a5fc5251093a
KAPOCODE <pokaaimejunior9@yahoo.com>
2021-10-20 16:28:13 +0200

    Ajoutez des fichiers projet.

---
+++ MyLibsNet.Serialisation/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace MyLibsNet.Serialisation
+{
+    public class Class1
+    {
+        public class serializer
+        {
+            public void Serialize<T>(string path, T data)
+            {
+                using (StreamWriter file = new StreamWriter(path, true))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(file.BaseStream, data);
+                }
+            }
+            public T Deserialize<T>(string path)
+            {
+                using (StreamReader file = new StreamReader(path, true))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    return (T)bf.Deserialize(file.BaseStream);
+                }
+            }
+
+        }
+    }
+}
